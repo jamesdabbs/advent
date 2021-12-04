ad56a595c655283209aeeec25fb7f5a18f545ab9
James Dabbs <james.dabbs@gmail.com>
2021-12-03 17:35:41 -0800

    streamline solution for day 3
    by modeling the input as [Int] and using bitwise
    operations instead of (Matrix Bool).

--- src/Problems/Y2021/D03/Solution.hs
+++ src/Problems/Y2021/D03/Solution.hs
@@ -1,57 +1,49 @@
 module Problems.Y2021.D03.Solution (solution) where
 
 import Import
-import Data.Attoparsec.Text (digit, many')
+import Data.Attoparsec.Text (digit, many', sepBy1)
 import Data.List (partition)
 import qualified Data.Matrix as Matrix
 import qualified Data.Vector as Vector
 
-solution :: Solution (Matrix Bool) Int
-solution = Solution parse $ pure . (part1 &&& part2)
+data Input = Input
+  { width :: Int
+  , values :: [Int]
+  }
 
-parse :: Parser (Matrix Bool)
-parse = fmap Matrix.fromLists . lines . many' $
-  "0" $> False <|>
-  "1" $> True
+solution :: Solution Input (Maybe Int)
+solution = Solution parse $ pure . (Just . part1 &&& part2)
+
+parse :: Parser Input
+parse = do
+  let bit = ("0" $> 0 <|> "1" $> 1)
+  values@(first:_) <- many' bit `sepBy1` "\n"
+  return $ Input (length first) (map binpack values)
+
+binpack :: [Int] -> Int
+binpack = reverse
+  >>> zip [0..]
+  >>> map (\(i, v) -> v * (2^i))
+  >>> sum
 
 -- 281 * 3814 = 1071734
-part1 :: Matrix Bool -> Int
-part1 input = uncurry (*) $ go 0 (0, 0)
+part1 :: Input -> Int
+part1 (Input width values) = uncurry (*) $ foldl f (0, 0) [0 .. width - 1]
   where
-    max = Matrix.ncols input
-
-    go n (gamma, epsilon) = case Matrix.safeGetCol (max - n) input of
-      Just col -> go (n + 1) $
-        if moreTrues col
-          then (gamma + 2 ^ n, epsilon)
-          else (gamma, epsilon + 2 ^ n)
-      _ -> (gamma, epsilon)
-
-moreTrues :: Vector Bool -> Bool
-moreTrues input =
-  let (ones, zeros) = Vector.partition identity input
-   in Vector.length ones >= Vector.length zeros
+    f (gamma, epsilon) i =
+      let (zeros, ones) = partition (== 0) $ map (.&. bit i) values
+      in if length ones > length zeros
+           then (gamma .|. bit i, epsilon)
+           else (gamma, epsilon .|. bit i)
 
 -- 1679 * 3648 = 6124992
-part2 :: Matrix Bool -> Int
-part2 = rows
-  >>> (go (>=) 0 &&& go (<) 0)
-  >>> uncurry (*)
+part2 :: Input -> Maybe Int
+part2 (Input width values) = (*)
+  <$> go (>=) width values
+  <*> go (< ) width values
   where
-    go :: (Int -> Int -> Bool) -> Int -> [Vector Bool] -> Int
+    go _ _ [value] = Just value
+    go _ 0 _ = Nothing
     go cmp i vs =
-      let (ones, zeros) = partition (\v -> v Vector.! i) vs
-          next = if length ones `cmp` length zeros then ones else zeros
-      in case next of
-           [vector] -> toInt vector
-           [] -> -1
-           rest -> go cmp (i + 1) rest
-
-rows :: Matrix a -> [Vector a]
-rows m = map (flip Matrix.getRow m) [1 .. Matrix.nrows m]
-
-toInt :: Vector Bool -> Int
-toInt v = Vector.foldl f 0 $ Vector.indexed $ Vector.reverse v
-  where
-    f acc (i, True) = acc + 2 ^ i
-    f acc _ = acc
+      let (ones, zeros) = partition (`testBit` (i - 1)) vs
+      in go cmp (i - 1) $ if length ones `cmp` length zeros then ones else zeros
